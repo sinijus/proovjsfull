848e5bcd931ea74a1d7b7fea52f8ecc08ebbb996
Jaanus <134766023+sinijus@users.noreply.github.com>
2024-02-06 11:09:15 +0200

    Setting up project

--- backend/Database/3_import_contacts.sql
+++ backend/Database/3_import_contacts.sql
@@ -1,6 +1,9 @@
 INSERT INTO smitproovjs.contact (id, first_name, last_name, codename, phone) VALUES (default,'Isabella', 'Hawthorne', 'IZ', '33728016443');
 INSERT INTO smitproovjs.contact (id, first_name, last_name, codename, phone) VALUES (default,'Maxwell', 'Sterling', 'xs', '3547392700');
 INSERT INTO smitproovjs.contact (id, first_name, last_name, codename, phone) VALUES (default,'Seraphina', 'Cruz', 'James', '46349027446');
+INSERT INTO smitproovjs.contact (id, first_name, last_name, codename, phone) VALUES (default,'Samantha ', 'Rodriguez ', 'Shadowfox', '33349027446');
+INSERT INTO smitproovjs.contact (id, first_name, last_name, codename, phone) VALUES (default,'Benjamin ', 'Chang ', 'Nightfall', '49349027446254');
+INSERT INTO smitproovjs.contact (id, first_name, last_name, codename, phone) VALUES (default,'Emily ', 'Patel ', 'Eclipse_Star', '34349027446');
 
 
 
